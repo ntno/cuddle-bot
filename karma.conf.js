f8af737d08cc513cf7a2ce80127377536afc324d
ntno <organick.natan@gmail.com>
2017-05-24 21:32:16 -0400

    initial copy from learnpod

---
+++ karma.conf.js
@@ -0,0 +1,87 @@
+// Karma configuration
+// Generated on Sun May 14 2017 00:01:30 GMT-0400 (EDT)
+
+module.exports = function(config) {
+  var root = "cuddlebot"
+  ,srcCode = root + "/src/js/*.js"
+  ,testCode = root + "/test/js/*.spec.js"
+  ,testCoverage = root + "/test/coverage/"
+  ,preprocessorConfig = {};
+  preprocessorConfig[srcCode] = 'coverage';
+
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-resource/angular-resource.min.js',
+      'node_modules/angular-route/angular-route.min.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      srcCode,
+      testCode
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: preprocessorConfig,
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : testCoverage
+    },
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
